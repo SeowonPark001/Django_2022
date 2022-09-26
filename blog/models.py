cb83115b257e9c3a9a38b9b4729a832e753a34e6
SeowonPark001 <qkrtjdnjs0531@gmail.com>
2022-09-27 03:28:01 +0900

    Post 모델 생성, 시간 설정

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+    create_at = models.DateTimeField(auto_now_add=True) # 새로 작성 시 auto_now_add
+    update_at = models.DateTimeField(auto_now=True)     # 수정(업데이트) 시 auto_now
+    #author: 추후 작성 예정
+
+    def __str__(self):
+        # {self.pk} : 해당 포스트 pk 값 // {self.title} : 해당 포스트의 title 값값
+       return f'[{self.pk}] {self.title}    {self.create_at}'   # ex) [1] Post_title_1
