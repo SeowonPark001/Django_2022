c2338625522919a0a6232831edd59ff8d5add6de
SeowonPark001 <qkrtjdnjs0531@gmail.com>
2022-10-18 15:32:03 +0900

    포스트 상세페이지 테스트하기

--- blog/tests.py
+++ blog/tests.py
@@ -9,6 +9,7 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    # 블로그 목록 페이지 테스트
     def test_post_list(self):
         # 1-1. 포스트 목록 페이지 가져오기
         response = self.client.get('/blog/', follow=True) # 301?
@@ -59,3 +60,36 @@ class TestView(TestCase):
 
         # 3-4. '아직 게시물이 없습니다' 문구는 더 이상 나타나지 X
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+
+    # 포스트 상세 페이지 테스트
+    def test_post_detail(self):
+        # 포스트 생성
+        post_001 = Post.objects.create(title='첫번째 포스트', content='첫번째 포스트입니다.')
+        # 포스트 경로가 맞는지 확인
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # 포스트 상세 페이지 가져오기
+        #response = self.client.get('/blog/1/', follow=True)
+        response = self.client.get(post_001.get_absolute_url(), follow=True)
+        # 응답 코드 확인
+        self.assertEqual(response.status_code, 200)
+        # soup 변수에 html 파일 담기?
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # soup의 navbar 태그 >> 텍스트에 Blog, About Me 가 있는지
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text) # post_detail.html 과 내용 맞추기 >> 안 그러면 ERROR
+
+        # 웹문서의 타이틀이 올바른지 확인 : html 문서의 title 안에 포함되어 있는지
+        self.assertIn(post_001.title, soup.title)
+
+        # id로 연결한 div 태그 찾기 - Main, Post, Comment
+        main_area = soup.find('div', id='main-area')
+        post_area = soup.find('div', id='post-area')
+        comment_area = soup.find('div', id='comment-area')
+        # 포스트 타이틀&내용이 해당 텍스트 부분에 있는지 확인
+        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
+
